be796b296765575bb1de6450a62eca64a415137c
leeeyoo <leeeyoo@gmail.com>
2023-11-30 02:03:56 +0900

    feat: navbar

---
+++ app/components/nav/NavBar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Container from "../Container";
+import { Oleo_Script_Swash_Caps } from 'next/font/google'
+
+const oleo = Oleo_Script_Swash_Caps({ subsets: ['latin'], weight: ['400', '700']  })
+
+const NavBar = () => {
+  return ( 
+    <div className="
+     sticky
+     top-0
+     w-full
+     bg-neutral-200
+     z-30
+     shadow-sm
+    "
+    >
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Link href="/" className={`${oleo.className} font-bold text-2xl`}>Minus 4 Degrees</Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>Cart</div>
+              <div>UserMenu</div>
+            </div>           
+          </div>
+        </Container>
+      </div>
+    </div>
+   );
+}
+ 
+export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main>E-Shop</main>
+    <div>E-Shop</div>
   )
 }
