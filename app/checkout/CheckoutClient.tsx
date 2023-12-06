62405ee42bb178d2a2aff80eaa01a504da9e56a0
leeeyoo <leeeyoo@gmail.com>
2023-12-06 23:54:27 +0900

    feat: payment

---
+++ app/checkout/CheckoutClient.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useCart } from "@/hooks/useCart"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
+
+const CheckoutClient = () => {
+  const cartProducts = useCart()
+  const [ loading, setLoading ] = useState(false)
+  const [ error, setError ] = useState(false)
+  const [ clientSecret, setClientSecret ] = useState("")
+
+  const router = useRouter()
+
+  useEffect(() => {
+    // Create a paymentintent as soon as the page loads
+    if (cartProducts) {
+      setLoading(true)
+      setError(false)
+
+      fetch("/api/create-payment-intent", {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({
+          items: cartProducts
+          payment_intent_id: paymentIntent
+        })
+      }).then((res) => {
+        setLoading(false)
+        if (res.status === 401) {
+          return router.push("/login")
+        }
+        return res.json()
+      }).then((data) => {
+        setClientSecret(data.paymentIntent.client_secret)
+        handleSetPaymentIntent(data.paymentIntent.id)
+      }).catch(() => {
+        setError(true)
+        console.log("Error", error)
+        toast.error("Something went wrong")
+      })
+    }
+  }, [cartProducts, paymentIntent])
+  return ( 
+    <>Checkout</>
+   );
+}
+ 
+export default CheckoutClient;
