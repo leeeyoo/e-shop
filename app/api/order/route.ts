9f27bb6cb4c324ca3be65ea7a8b12b84a0419db1
leeeyoo <leeeyoo@gmail.com>
2023-12-20 15:58:43 +0900

    feat: orders

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -5,9 +5,8 @@ import { getCurrentUser } from "@/actions/getCurrentUser"
 export async function PUT(request: Request) {
   const currentUser = await getCurrentUser()
 
-  if (!currentUser || currentUser.role !== "ADMIN") {
-    return NextResponse.error()
-  }
+  if (!currentUser) return NextResponse.error()
+  if (currentUser.role !== "ADMIN") return NextResponse.error()
 
   const body = await request.json()
   const { id, deliveryStatus } = body
