bd1f584dbb1d06d3a851408cbfca6716ee96e164
leeeyoo <leeeyoo@gmail.com>
2023-12-22 11:15:18 +0900

    feat: bar graph

--- app/admin/BarGraph.tsx
+++ app/admin/BarGraph.tsx
@@ -18,9 +18,32 @@ type GraphData = {
 const BarGraph: React.FC<BarGraphProps> = ({
   data
 }) => {
-  console.log(data)
+  const labels = data.map(item => item.day)
+  const amounts = data.map(item => item.totalAmount)
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Sale Amount",
+        data: amounts,
+        backgroundColor: "rgba(75, 192, 192, 1)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1
+      }
+    ]
+  }
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+
   return (
-    <></>
+    <Bar data={chartData} options={options}></Bar>
   );
 }
 
